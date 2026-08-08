b89150bf0f7b6787a3792eea3192612858497689
Adityadi195 <adityadi230@gmail.com>
2026-08-08 12:38:47 +0700

    feat: initialize Spring Boot project

---
+++ finalproject/src/test/java/com/dibimbing/finalproject/antrigo/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibimbing.finalproject.antrigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
